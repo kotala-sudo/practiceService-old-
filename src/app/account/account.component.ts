94b1a278893ae117a20989257565b5d7a051e7c3
kotala-sudo <pkotala@yahoo.com>
2022-04-01 09:25:00 -0500

    2 files modified

--- src/app/account/account.component.ts
+++ src/app/account/account.component.ts
@@ -1,9 +1,9 @@
-import { Component, EventEmitter, Input, Output } from "@angular/core";
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
-  selector: "app-account",
-  templateUrl: "./account.component.html",
-  styleUrls: ["./account.component.css"]
+  selector: 'app-account',
+  templateUrl: './account.component.html',
+  styleUrls: ['./account.component.css'],
 })
 export class AccountComponent {
   @Input() account: { name: string; status: string };
@@ -15,6 +15,6 @@ export class AccountComponent {
 
   onSetTo(status: string) {
     this.statusChanged.emit({ id: this.id, newStatus: status });
-    console.log(`Status changed for "${this.account.name}", new status is: "${status}"`);
+    console.log(`A status change occured, the new status is ${status}`);
   }
 }
--- src/app/new-account/new-account.component.ts
+++ src/app/new-account/new-account.component.ts
@@ -1,9 +1,9 @@
-import { Component, EventEmitter, Output } from "@angular/core";
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
-  selector: "app-new-account",
-  templateUrl: "./new-account.component.html",
-  styleUrls: ["./new-account.component.css"]
+  selector: 'app-new-account',
+  templateUrl: './new-account.component.html',
+  styleUrls: ['./new-account.component.css'],
 })
 export class NewAccountComponent {
   @Output() accountAdded = new EventEmitter<{ name: string; status: string }>();
@@ -11,8 +11,8 @@ export class NewAccountComponent {
   onCreateAccount(accountName: string, accountStatus: string) {
     this.accountAdded.emit({
       name: accountName,
-      status: accountStatus
+      status: accountStatus,
     });
-    console.log(`A new account "${accountName}" created with status: ${accountStatus}`);
+    console.log(`A status change occured, the new status is ${accountStatus}`);
   }
 }
