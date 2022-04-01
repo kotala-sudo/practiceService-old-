354e844d66d881914f53d881def708ea8fc76a2b
kotala-sudo <pkotala@yahoo.com>
2022-04-01 09:25:23 -0500

    1 file modified

--- src/app/account/account.component.ts
+++ src/app/account/account.component.ts
@@ -8,10 +8,7 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 export class AccountComponent {
   @Input() account: { name: string; status: string };
   @Input() id: number;
-  @Output() statusChanged = new EventEmitter<{
-    id: number;
-    newStatus: string;
-  }>();
+  @Output() statusChanged = new EventEmitter<{id: number;newStatus: string;}>();
 
   onSetTo(status: string) {
     this.statusChanged.emit({ id: this.id, newStatus: status });
